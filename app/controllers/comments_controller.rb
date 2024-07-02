937bf90a88507af7e4033da5c7a16213f2b13e92
Artem134134 <car322132@gmail.com>
2024-07-02 20:51:17 +0300

    Был создан контроллер comments и шаблон для CRUD

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+
+	 def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def comment_params
+  	params.require(:comment).permit(:name, :text, :date)
+  end
+end
