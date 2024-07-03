ec319cb2e15607d050580f30948e02632bb3f137
Artem134134 <car322132@gmail.com>
2024-07-03 15:00:11 +0300

    Пишем контроллеры по паттерну CRUD в comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,45 @@
 class CommentsController < ApplicationController
 
 	 def index
+    @comments = Comment.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to comments_path
+    else
+      rander :new
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to @comment
+    else
+      rander :edit
+    end  
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+      redirect_to comments_path
   end
+  
 
   private 
 
